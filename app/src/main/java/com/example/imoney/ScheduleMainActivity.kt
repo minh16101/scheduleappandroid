58a0e912a2e629576ede1934f20ccd83a2f570c5
Minh Hoang Nhat <Admin@LAPTOP-F6J06I0S.(none)>
2024-01-13 11:25:50 +0700

    create project

---
+++ app/src/main/java/com/example/imoney/ScheduleMainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.imoney
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ScheduleMainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_schedule_main)
+    }
+}
